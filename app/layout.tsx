0482d68768bfa5b855edde06958045677688f31d
kartik-270 <kartik2310179@akgec.ac.in>
2026-08-09 11:46:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import GlobalOneTap from './components/GlobalOneTap';
 import { Inter, Manrope } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -135,6 +136,14 @@ export default function RootLayout({
           </TierProvider>
           <Analytics />
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
+          <Script id="aclib" src="//acscdn.com/script/aclib.js" strategy="afterInteractive" />
+          <Script id="aclib-init" strategy="afterInteractive">
+            {`
+              aclib.runAutoTag({
+                  zoneId: 'isleobkjgr',
+              });
+            `}
+          </Script>
         </ThemeProvider>
       </body>
     </html>
